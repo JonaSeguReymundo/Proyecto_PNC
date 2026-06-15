be8e86c0ddc34f3e79ae6cee84038d4c3c0b6120
JonaSeguReymundo <00043823@uca.edu.sv>
2026-06-15 01:00:53 -0600

    chore: setup initial project configuration and build scripts

---
+++ warehouse-inventory-api/src/test/java/com/example/warehouseinventoryapi/WarehouseInventoryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.warehouseinventoryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WarehouseInventoryApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
